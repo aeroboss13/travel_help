70b97fce4e0ec62dfcb3b0e2aba6d91818a75ef4
Pavel Bardin <61287279+aeroboss13@users.noreply.github.com>
2023-11-06 13:51:35 +0300

    Добавлена БД для записи рекомендаций и записи учетных данных пользователей

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from tkinter import PhotoImage
 from PIL import Image, ImageTk
 import webbrowser
 from ttkthemes import ThemedTk
+import sqlite3
 
 # Создаем окно приложения с использованием темы
 app = ThemedTk(theme="radiance")
@@ -14,10 +14,27 @@ app.geometry("800x600")
 # Переменная для отслеживания текущей темы
 current_theme = "radiance"
 
-# База данных для хранения пользователей (просто для примера)
-users = {'user1': 'password1', 'user2': 'password2'}
+# Создаем подключение к базе данных SQLite
+db_connection = sqlite3.connect("travel_app.db")
+db_cursor = db_connection.cursor()
 
-# Создаем словарь с рекомендациями по безопасности для различных стран
+# Создаем таблицу для рекомендаций по безопасности, если ее нет
+db_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS safety_recommendations (
+        country TEXT PRIMARY KEY,
+        recommendations TEXT
+    )
+''')
+
+# Создаем таблицу для пользователей, если ее нет
+db_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT
+    )
+''')
+
+# Добавляем рекомендации по безопасности в базу данных
 safety_recommendations = {
     'Страна 1': "Рекомендации для Страны 1:\n"
                 "- Поддерживайте местные законы и обычаи.\n"
@@ -29,6 +46,25 @@ safety_recommendations = {
                 "- Перед поездкой уточните информацию о местной медицинской помощи и вакцинациях.\n"
                 "- Соблюдайте осторожность при посещении отдаленных мест и предупреждайте кого-то о вашем маршруте."
 }
+
+for country, recommendations in safety_recommendations.items():
+    db_cursor.execute('INSERT OR REPLACE INTO safety_recommendations (country, recommendations) VALUES (?, ?)',
+                      (country, recommendations))
+
+db_connection.commit()
+
+
+# Функция для добавления пользователя в базу данных
+def add_user_to_db(username, password):
+    db_cursor.execute('INSERT OR REPLACE INTO users (username, password) VALUES (?, ?)', (username, password))
+    db_connection.commit()
+
+
+# Функция для проверки учетных данных пользователя
+def check_user_credentials(username, password):
+    db_cursor.execute('SELECT password FROM users WHERE username = ?', (username,))
+    result = db_cursor.fetchone()
+    return result[0] if result and result[0] == password else None
 
 
 # Определение функций для функциональности приложения
@@ -54,7 +90,7 @@ def register_user():
         username = username_entry.get()
         password = password_entry.get()
         if username and password:
-            users[username] = password
+            add_user_to_db(username, password)
             registration_window.destroy()
             messagebox.showinfo("Регистрация", "Пользователь зарегистрирован")
         else:
@@ -85,11 +121,14 @@ def login_user():
     def authenticate():
         username = username_entry.get()
         password = password_entry.get()
-        if username in users and users[username] == password:
-            login_window.destroy()
-            messagebox.showinfo("Авторизация", "Вход выполнен успешно")
+        if username and password:
+            if check_user_credentials(username, password):
+                login_window.destroy()
+                messagebox.showinfo("Авторизация", "Вход выполнен успешно")
+            else:
+                messagebox.showerror("Ошибка", "Неправильный логин или пароль")
         else:
-            messagebox.showerror("Ошибка", "Неправильный логин или пароль")
+            messagebox.showerror("Ошибка", "Заполните все поля")
 
     login_button = ttk.Button(login_window, text="Войти", command=authenticate)
     login_button.pack()
@@ -119,6 +158,12 @@ def open_travel_chat():
     send_button.pack()
 
 
+def get_safety_recommendations_from_db(country):
+    db_cursor.execute('SELECT recommendations FROM safety_recommendations WHERE country = ?', (country,))
+    result = db_cursor.fetchone()
+    return result[0] if result else "Нет данных о рекомендациях по безопасности для выбранной страны"
+
+
 def get_safety_recommendations():
     safety_window = tk.Toplevel(app)
     safety_window.title("Рекомендации по безопасности")
@@ -127,27 +172,33 @@ def get_safety_recommendations():
     safety_label = tk.Label(safety_window, text="Выберите страну и получите рекомендации по безопасности:")
     safety_label.pack()
 
-    countries = ['Страна 1', 'Страна 2', 'Страна 3']  # Пример списка стран
+    # Запрос всех доступных стран из базы данных
+    db_cursor.execute('SELECT country FROM safety_recommendations')
+    countries = [row[0] for row in db_cursor.fetchall()]
 
     country_var = tk.StringVar()
-    country_var.set(countries[0])
+    country_var.set(countries[0] if countries else "")
 
-    country_dropdown = ttk.Combobox(safety_window, textvariable=country_var, values=countries)
-    country_dropdown.pack()
+    if countries:
+        country_dropdown = ttk.Combobox(safety_window, textvariable=country_var, values=countries)
+        country_dropdown.pack()
 
-    safety_text = tk.Text(safety_window, wrap=tk.WORD)
-    safety_text.pack()
+        safety_text = tk.Text(safety_window, wrap=tk.WORD)
+        safety_text.pack()
 
-    def show_recommendations():
-        selected_country = country_var.get()
-        recommendations = safety_recommendations.get(selected_country, "Рекомендации отсутствуют")
-        safety_text.config(state="normal")
-        safety_text.delete("1.0", "end")
-        safety_text.insert("end", recommendations)
-        safety_text.config(state="disabled")
+        def show_recommendations():
+            selected_country = country_var.get()
+            recommendations = get_safety_recommendations_from_db(selected_country)
+            safety_text.config(state="normal")
+            safety_text.delete("1.0", "end")
+            safety_text.insert("end", recommendations)
+            safety_text.config(state="disabled")
 
-    show_button = ttk.Button(safety_window, text="Получить рекомендации", command=show_recommendations)
-    show_button.pack()
+        show_button = ttk.Button(safety_window, text="Получить рекомендации", command=show_recommendations)
+        show_button.pack()
+    else:
+        no_data_label = tk.Label(safety_window, text="Нет данных о рекомендациях по безопасности")
+        no_data_label.pack()
 
 
 def open_virtual_tours():
@@ -207,8 +258,32 @@ virtual_tours_button.pack()
 
 # Виджет-ползунок для переключения темы
 theme_toggle_button = ttk.Button(app, text="Сменить тему", command=toggle_theme)
-theme_toggle_button.pack(side="top")
+theme_toggle_button.pack()
+
+# Добавляем изображение на вкладки
+image_path = "travel_image.jpg"  # Замените на путь к изображению
+image = Image.open(image_path)
+photo = ImageTk.PhotoImage(image)
+
+image_label_1 = tk.Label(registration_tab, image=photo)
+image_label_1.image = photo
+image_label_1.pack()
+
+image_label_2 = tk.Label(chat_tab, image=photo)
+image_label_2.image = photo
+image_label_2.pack()
+
+image_label_3 = tk.Label(safety_recommendations_tab, image=photo)
+image_label_3.image = photo
+image_label_3.pack()
+
+image_label_4 = tk.Label(virtual_tours_tab, image=photo)
+image_label_4.image = photo
+image_label_4.pack()
 
 tab_control.pack(expand=1, fill="both")
 
 app.mainloop()
+
+# Закрываем соединение с базой данных при выходе из приложения
+db_connection.close()
